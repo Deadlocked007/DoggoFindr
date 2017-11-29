cdc7c186e82b56d378a7e3db9e02b4794baa37c7
Woosung Kim <wskim012@gmail.com>
2017-11-29 04:52:18 -0500

    Creaetd Base template html page, updated url

--- WebApp/doggofindr/urls.py
+++ WebApp/doggofindr/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from . import views
+from .views import(
+    home,
 
+)
 
 
 #For Web URL configuration
 app_name = 'doggofindr'
 urlpatterns = [
-
+    url(r'^$', home, name='home'),
 ]
--- WebApp/doggofindr/views.py
+++ WebApp/doggofindr/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
+from django.template.context_processors import csrf
+from django.http import HttpResponseRedirect, HttpResponse
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth.models import User
+from .models import Image
+from.forms import ImageForm
+
+def home(request):
+    imgs = Image.objects.all().order_by('-id')
+    user = request.user
+    context = {
+        "imgList" : imgs,
+        "user": user,
+    }
+    return render(request, 'doggofindr/index.html',context)
